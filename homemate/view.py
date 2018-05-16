96e945413cd01c88542b5d9df8018f91ee9592ff
122042 <2676902@qq.com>
2018-05-16 19:30:00 +0800

    fix wrong spelling

--- homemate/view.py
+++ homemate/view.py
@@ -1,11 +1,11 @@
 
 # -*- coding: utf-8 -*-
- 
+
 #from django.http import HttpResponse
 from django.shortcuts import render
- 
+
 def hello(request):
     context          = {}
-    context['welome'] = 'Welcome To!'
+    context['welcome'] = 'Welcome To!'
     return render(request, 'homepage.html', context)
 
